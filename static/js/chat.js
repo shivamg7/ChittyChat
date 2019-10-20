f164a6c9a517137fdd5c147ca7f194a072d89616
Shivam Gupta <shivamgupta@Shivams-MacBook-Air.local>
2019-10-20 16:34:02 +0530

    Changed encyrption scheme

--- static/js/chat.js
+++ static/js/chat.js
@@ -30,6 +30,27 @@ function send(sender, receiver, message) {
     })
 }
 
+
+function setCookie(cname, cvalue) {
+    document.cookie = cname + "=" + cvalue + ";" + "path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
+
 function receive() {
     $.get('/api/messages/' + sender_id + '/' + receiver_id, function (data) {
         console.log(data);
